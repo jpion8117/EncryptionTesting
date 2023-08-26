bc5f915598bf595216787ad91395a19d8dfd2c47
jp8117 <jp8117@live.com>
2023-08-26 07:27:29 -0400

    Add project files.

---
+++ PasswordHashing/PasswordMagic.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Text.Json;
+
+namespace PasswordHashing
+{
+    internal class PasswordMagic
+    {
+        private string _unencryptedPw = "";
+        private string _passwordHash = "";
+        static private UnicodeEncoding _encoding = new UnicodeEncoding();
+        
+        public PasswordMagic(string unencryptedPw)
+        {
+            UnencryptedPw = unencryptedPw;
+        }
+        public string PasswordHash
+        {
+            get { return _passwordHash; }
+        }
+        public string UnencryptedPw
+        {
+            set
+            {
+                _unencryptedPw = value;
+
+                using (SHA512 shaM = SHA512.Create())
+                {
+                    var toEncrypt = Encoding.Unicode.GetBytes(_unencryptedPw);
+                    var hash = shaM.ComputeHash(toEncrypt);
+
+                    foreach (byte b in hash)
+                    {
+                        _passwordHash += String.Format("{0:x2}", b);
+                    }
+                }
+            }
+            get => _unencryptedPw;
+        }
+        public static void GenerateKey(out string key, out string initializationVector)
+        {
+            using (Aes aesAlgorithm = Aes.Create())
+            {
+                key = Convert.ToBase64String(aesAlgorithm.Key);
+                initializationVector = Convert.ToBase64String(aesAlgorithm.IV);
+            }
+        }
+
+        static public string EncryptAES(string plaintext, string key, string initializationVector)
+        {
+            string cyphertext = "";
+
+            using (Aes aesAlgorithm = Aes.Create())
+            {
+                aesAlgorithm.Key = Convert.FromBase64String(key);
+                aesAlgorithm.IV = Convert.FromBase64String(initializationVector);
+                ICryptoTransform encryptor = aesAlgorithm.CreateEncryptor();
+                
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cryptostream = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter writer = new StreamWriter(cryptostream))
+                        {
+                            writer.Write(plaintext);
+                        }
+
+                        cyphertext = Convert.ToBase64String(ms.ToArray());
+                    }
+                }
+            }
+
+            return cyphertext;
+        }
+        static public string DecryptAES(string cyphertext, string key, string initializationVector)
+        {
+            string plaintext = "";
+
+            using (Aes aesAlgorithm = Aes.Create())
+            {
+                aesAlgorithm.Key = Convert.FromBase64String(key);
+                aesAlgorithm.IV = Convert.FromBase64String(initializationVector);
+                ICryptoTransform decryptor = aesAlgorithm.CreateDecryptor();
+
+                using (MemoryStream memory = new MemoryStream(Convert.FromBase64String(cyphertext)))
+                {
+                    using (CryptoStream cryptostream = new CryptoStream(memory, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader reader = new StreamReader(cryptostream))
+                        {
+                            plaintext = reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return plaintext;
+        }
+
+        //static public string EncryptRSA(string plaintext, string key)
+        //{
+        //    using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+        //    {
+        //        var jsonOptions = new JsonSerializerOptions();
+        //        jsonOptions.IncludeFields = true;
+
+        //        RSAParameters param = JsonSerializer.Deserialize<RSAParameters>(key, jsonOptions);
+        //        rsa.ImportParameters(param);
+
+        //        var encryptedText = rsa.Encrypt(_encoding.GetBytes(plaintext), false);
+
+        //        return _encoding.GetString(encryptedText);
+        //    }
+        //}
+        //static public string DecryptRSA(string encryptedText, string key)
+        //{
+        //    using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+        //    {
+        //        var jsonOptions = new JsonSerializerOptions();
+        //        jsonOptions.IncludeFields = true;
+
+        //        RSAParameters param = JsonSerializer.Deserialize<RSAParameters>(key, jsonOptions);
+
+        //        rsa.ImportParameters(param);
+
+        //        var plainText = rsa.Decrypt(_encoding.GetBytes(encryptedText), false);
+
+        //        return _encoding.GetString(plainText);
+        //    }
+        //}
+        public bool Validate(string pwHash)
+        {
+            if (pwHash == PasswordHash)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ PasswordHashing/Program.cs
@@ -0,0 +1,97 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PasswordHashing;
+using System.Security.Cryptography;
+
+int parsedInput = -1;
+while (parsedInput != 0)
+{
+    Console.Clear();
+    Console.WriteLine("1. New Password");
+    Console.WriteLine("2. Use Password");
+    Console.WriteLine("3. Encrypt Statement");
+    Console.WriteLine("4. View Encrypted Statement");
+    Console.WriteLine("5. Decrypt and View Statement");
+    Console.WriteLine("\n0. Quit");
+    Console.Write("\nMake Selection: ");
+    var input = Console.ReadLine();
+    int.TryParse(input, out parsedInput);
+
+    if (parsedInput == 1)
+    {
+        Console.Clear();
+        Console.Write("Please enter a new password: ");
+        input = Console.ReadLine();
+
+        if (input != null)
+        {
+            var password = new PasswordMagic(input);
+            File.WriteAllText("superSecurePassword.txt", password.PasswordHash);
+        }
+    }
+    else if (parsedInput == 2)
+    {
+        Console.Clear();
+        Console.Write("Please enter the password: ");
+        input = Console.ReadLine();
+
+        if (input != null)
+        {
+            var password = new PasswordMagic(input);
+            var passwordHash = File.ReadAllText("superSecurePassword.txt");
+            if (password.Validate(passwordHash))
+                Console.Write("\n\nAccess Granted");
+            else
+                Console.Write("\n\nAccess Denied");
+            Console.Write(", Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+    else if (parsedInput == 3)
+    {
+        Console.Clear();
+        Console.Write("Please write a statement to encrypt: ");
+        input = Console.ReadLine();
+
+        PasswordMagic.GenerateKey(out var key, out var initializationVector);
+        File.WriteAllText("AESKey.txt", key);
+        File.WriteAllText("AESIV.txt", initializationVector);
+
+        var encryptedText = "";
+        if (input != null)
+            encryptedText = PasswordMagic.EncryptAES(input, key, initializationVector);
+
+        File.WriteAllText("encrypted.txt", encryptedText);
+    }
+    else if (parsedInput == 4)
+    {
+        Console.Clear();
+        Console.WriteLine("Encrypted Text: \n");
+
+        var encryptedText = File.ReadAllText("encrypted.txt");
+        Console.WriteLine("\n" + encryptedText);
+
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+    else if (parsedInput == 5)
+    {
+        Console.Clear();
+        Console.WriteLine("Encrypted Text: \n");
+
+        var encryptedText = File.ReadAllText("encrypted.txt");
+        Console.WriteLine(encryptedText);
+
+        var key = File.ReadAllText("AESKey.txt");
+        var initializationVector = File.ReadAllText("AESIV.txt");
+
+        var decryptedText = PasswordMagic.DecryptAES(encryptedText, key, initializationVector);
+
+        Console.WriteLine("\nDecrypted Text: \n");
+
+        Console.WriteLine(decryptedText);
+
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+
+}
